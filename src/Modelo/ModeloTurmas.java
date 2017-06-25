9260a51a2ffac567da8dbd513ceac4345b0284e5
Rogerio <Rogerio@Rogerio-PC>
2017-06-25 15:31:44 -0300

    Envio Modelo Turmas

--- src/Modelo/ModeloTurmas.java
+++ src/Modelo/ModeloTurmas.java
@@ -5,18 +5,86 @@
  */
 package Modelo;
 
+import java.util.Date;
+
 /**
  * 
  * 
-idTurmas     INT NOT NULL,
-idProf       INT NOT NULL,
-idCurso      INT NOT NULL,
-dataInicio   DATE NULL,
-dataFinal    DATE NULL,
-cargaHora   SMALLINT,
  *
  * @author Rogerio
  */
 public class ModeloTurmas {
+    private int idturmas;
+    private String nomeprof;
+    private String nomecurso;
+    private String nometurma;
+    private Date   datainicio;
+    private Date   datafinal;
+    private int    cargahora;
+    private String pesquisar;
+
+    public int getIdturmas() {
+        return idturmas;
+    }
+
+    public void setIdturmas(int idturmas) {
+        this.idturmas = idturmas;
+    }
+
+    public String getNomeprof() {
+        return nomeprof;
+    }
+
+    public void setNomeprof(String nomeprof) {
+        this.nomeprof = nomeprof;
+    }
+
+    public String getNomecurso() {
+        return nomecurso;
+    }
+
+    public void setNomecurso(String nomecurso) {
+        this.nomecurso = nomecurso;
+    }
+
+    public String getNometurma() {
+        return nometurma;
+    }
+
+    public void setNometurma(String nometurma) {
+        this.nometurma = nometurma;
+    }
+
+    public Date getDatainicio() {
+        return datainicio;
+    }
+
+    public void setDatainicio(Date datainicio) {
+        this.datainicio = datainicio;
+    }
+
+    public Date getDatafinal() {
+        return datafinal;
+    }
+
+    public void setDatafinal(Date datafinal) {
+        this.datafinal = datafinal;
+    }
+
+    public int getCargahora() {
+        return cargahora;
+    }
+
+    public void setCargahora(int cargahora) {
+        this.cargahora = cargahora;
+    }
+
+    public String getPesquisar() {
+        return pesquisar;
+    }
+
+    public void setPesquisar(String pesquisar) {
+        this.pesquisar = pesquisar;
+    }
     
 }
